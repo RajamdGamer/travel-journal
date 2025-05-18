3c10a94c8688131cfe3181d4ec868b039c104052
RajamdGamer <rajaincoc@gmail.com>
2025-05-18 23:50:14 +0530

    Commited : first commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,15 @@
+export default function Entry(){
+    return(
+        <article className="journalentry">
+            <div className="main-image-container">
+            <img className="main-image"src="https://www.stayvista.com/blog/wp-content/uploads/2024/08/Untitled-design-7.jpg" alt="Conoor"/>
+            </div>
+            
+            <div>
+                <img src="src/images/location.png" alt="gps marker icon"/>
+                <span>Conoor </span>
+                <a href="https://maps.app.goo.gl/X93RiBR9kE1DnfySA">View on Google maps.</a>
+            </div>
+        </article>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+       <header>
+        <img src="src/images/travel.png"></img>
+        <h1>Travel Journal...</h1>
+       </header>
+
+    )
+}
